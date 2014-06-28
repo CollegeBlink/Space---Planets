fa69420932ef482313710bb2b2d01c457ea8fd4a
dumesm1 <markdumes@Marks-MacBook-Air.local>
2014-06-28 11:59:47 -0500

    Initial Commit

---
+++ Space & Planets/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Space & Planets
+//
+//  Created by Mark Dumes on 6/28/14.
+//  Copyright (c) 2014 CollegeBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Space & Planets/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Space & Planets
+//
+//  Created by Mark Dumes on 6/28/14.
+//  Copyright (c) 2014 CollegeBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
